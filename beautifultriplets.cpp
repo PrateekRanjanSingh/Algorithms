032315f4265d0ed982aaa5c7b6d74615ea692529
16BIT0127Prateek <prateekranjansingh@gmail.com>
2018-12-27 21:27:24 +0530

    Final solution for beautiful triplets

--- beautifultriplets.cpp
+++ beautifultriplets.cpp
@@ -2,28 +2,6 @@
 
 using namespace std;
 
-vector<string> split_string(string);
-
-// Complete the beautifulTriplets function below.
-int beautifulTriplets(int d, vector<int> v) 
-{
-    int n  = v.size();
-    int a[n] = {0};
-    for(int i = 0;i<v.size();i++)
-    {
-        a[v[i]]++;
-    }
-    int count = 0;
-    // for(int i = 0;i<(v.size()-2*d);i++)
-    // {
-    //     if(a[i]>0)
-    //     if(a[i+d]>0)
-    //     if(a[i+2*d]>0) count++;
-    // }
-    for(int i = 0;i<v.size();i++) cout << a[i] << " ";
-    return count;
-}
-
 int main ()
 {
     int n;
@@ -37,21 +15,22 @@ int main ()
         cin >> x;
         v.push_back(x);
     }
-    //cout << beautifulTriplets(d,v);
-    int a[300000] = {0};
-    for(int i = 0;i<v.size();i++)
+    int a[100000] = {0};
+    for(int i = 0;i<n;i++)
     {
         a[v[i]]++;
     }
     int count = 0;
-    for(int i = 0;i<(n);i++)
+    for(int i = 0;i<100000;i++)
     {
         if(a[i]>0 && a[i+d]>0 && a[i+2*d]>0)
         {
-            int m = max(a[i],a[i+d]);
-            count += max(m,a[i+2*d]);
+            //int m = max(a[i],a[i+d]);
+            //count += max(m,a[i+2*d]);
+            count += a[i];
         }
     }
     cout << count;
     return 0;
 }
+
