2a96f2607f3453418ae58eaa95c2877288064d22
16BIT0127Prateek <prateekranjansingh@gmail.com>
2019-02-07 19:30:41 +0530

    Add code for letter combinations of a phone number

---
+++ letter_combinations_of_a_phone_number.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void fun(string ar[],string digits,int index, string s)
+{
+    if(index==digits.length())
+    {
+        cout << s << " ";
+        return;
+    }
+    int p = digits[index] - '0';
+    string letters = ar[p];
+    for(int i = 0;i<letters.length();i++)
+    {
+        fun(ar,digits,index+1,s+letters[i]);
+    }
+}
+
+int main() {
+	string digits;
+	cin >> digits;
+	string s ="";
+    string ar[]={"0","1","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    fun(ar,digits,0,s);
+	return 0;
+}
