ae1c22c577c1ad44d3f1d0a0edc6157c6c9c08e1
PrateekRanjanSingh <prateekranjansingh@gmail.com>
2019-04-07 22:55:50 +0530

    Add code for max sum dp

---
+++ maxsumdp.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int func(int n,int max)
+{
+    
+}
+
+int main() {
+	//code
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	    int n;
+	    cin >> n;
+	    cout << func(n,0) << endl;
+	}
+	return 0;
+}
