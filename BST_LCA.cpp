46c2ff2b6d4422deedf89eb4b4472f6f0f1d4313
PrateekRanjanSingh <prateekranjansingh@gmail.com>
2019-07-23 19:04:34 +0530

    Add code for Lowest Common Ancestor in a BST

---
+++ BST_LCA.cpp
@@ -0,0 +1,12 @@
+Node* LCA(Node *root, int n1, int n2)
+{
+   //Your code here
+   if(root==NULL) return NULL;
+   if(root->data == n1 || root->data == n2) return root;
+   Node *left_lca = LCA(root->left,n1,n2);
+   Node *right_lca = LCA(root->right,n1,n2);
+   if(left_lca && right_lca) return root;
+   if(left_lca!=NULL)
+   return left_lca;
+   else return right_lca;
+}
